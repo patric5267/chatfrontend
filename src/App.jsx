c14fa5e7710aab92959b53b3c17fc9bfb8cde52d
patric5267 <115068229+patric5267@users.noreply.github.com>
2024-03-03 19:19:31 +0530

    changed routr2

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ function App() {
     <Routes>
       <Route path='/' element={<Login/>}/>
       <Route path='/chat' element={<Chat/>}/>
+
     </Routes>
   </Router>
   )
--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -2,26 +2,26 @@ import React from 'react'
 import { useEffect, useMemo, useState } from 'react'
 import { Link, useLocation, useNavigate, useParams } from 'react-router-dom'
 import { io } from 'socket.io-client'
-import bg from '../assets/bg.png'
+import Background from '../assets/Background.png'
 import { IoIosSend } from "react-icons/io";
-import {motion}  from 'framer-motion'
+import { motion } from 'framer-motion'
 const Chat = () => {
     const navigate = useNavigate()
     const location = useLocation()
-    useEffect(()=>{
-       if(!location.state){
-        navigate("/")
-       }
-    },[location])
+    useEffect(() => {
+        if (!location.state) {
+            navigate("/")
+        }
+    }, [location])
     const [input, setInput] = useState("")
     const [message, setMessage] = useState([])
     const socket = useMemo(() =>
-        io("https://chatbackend-x9f5.onrender.com", {
+        io("https://chatbackend-33k0.onrender.com", {
             withCredentials: true
         })
         , [])
     const handleclick = () => {
-        socket.emit("message", { msg: input, name: location.state.name, img: location.state.image })
+        socket.emit("message", { msg: input, name: location.state.name})
         setInput("")
     }
     useEffect(() => {
@@ -29,35 +29,37 @@ const Chat = () => {
         element.scrollTop = element.scrollHeight;
     })
     useEffect(() => {
-        socket.on("connect", () => {
-            socket.emit("usersent", location.state.name)
-            socket.on("welcome", (msg) => {
-                setMessage((message) => [...message, { msg: msg, name: "server", align: "center" }])
-            })
-            socket.on("userjoined", (msg) => {
-                setMessage((message) => [...message, { msg: msg, name: "server", align: "center" }])
-            })
+      location.state &&  socket.emit("usersent", location.state.name)
+        socket.on("welcome", (msg1) => {
+            setMessage((message)=>[...message , {name:"server" , msg:msg1 , align:'center'} ])
+        })
+        socket.on("otheruserhasjoined", (msg2) => {
+            setMessage((message)=>[...message , {name:"server" , msg:msg2 , align:'center'} ])
         })
         socket.on("recieve-message", (data) => {
             if (socket.id === data.id) {
-                setMessage((message) => [...message, { msg: data.msg, name: data.img, align: "left" }])
+            setMessage((message)=>[...message , {name:location.state.image , msg:data.msg , align:'left'} ])
+
+                // console.log({ name: location.state.image, msg: data.msg });
             }
             else {
-                setMessage((message) => [...message, { msg: data.msg, name: data.img, align: "right" }])
-
+                setMessage((message)=>[...message , {name:location.state.image , msg:data.msg , align:'right'} ])
+                // console.log({ name: location.state.image, msg: data.msg });
             }
         })
-        socket.on("disconnect-message", (msg) => {
-            setMessage((message) => [...message, { msg: msg, name: "server", align: "center" }])
+        socket.on("disconnect-message", (data) => {
+            setMessage((message)=>[...message , {name:"server" , msg:data , align:'center'} ])
+            console.log(data);
         })
+
         return () => {
             socket.disconnect()
         }
     }, [socket])
     return (
         <>
-       <div className='maincontainer h-[100svh] relative' >
-                <img className='h-full w-full' src={bg} alt="" />
+            <div className='maincontainer h-[100svh] relative' >
+                <img className='h-full w-full' src={Background} alt="" />
                 <div className='chatcontainer absolute px-3 lg:px-0 flex w-full h-full  justify-center items-center    top-0 left-0'>
                     <div className='flex flex-col justify-center items-center  w-full md:w-[48rem]'>
                         <div className="goback w-full bg-slate-400 py-2 pl-1">
@@ -70,19 +72,19 @@ const Chat = () => {
                                     if (i.align === 'center') {
                                         return (
                                             <motion.div
-                                            initial={{
-                                                scale:0,
-                                                opacity:0
-                                            }}
-                                            animate={{
-                                                scale:1,
-                                                opacity:1
-                                            }}
-                                            transition={{
-                                                duration:1,
-                                                type:"spring"
-                                            }}
-                                            key={index} className=" my-2   flex justify-center   ">
+                                                initial={{
+                                                    scale: 0,
+                                                    opacity: 0
+                                                }}
+                                                animate={{
+                                                    scale: 1,
+                                                    opacity: 1
+                                                }}
+                                                transition={{
+                                                    duration: 1,
+                                                    type: "spring"
+                                                }}
+                                                key={index} className=" my-2   flex justify-center   ">
                                                 <p className=' text-white text-xl'>{i.msg}</p>
                                             </motion.div>
                                         )
@@ -90,19 +92,19 @@ const Chat = () => {
                                     else if (i.align === "right") {
                                         return (
                                             <motion.div
-                                            initial={{
-                                                x:"-100vw",
-                                                opacity:0
-                                            }}
-                                            animate={{
-                                                x:0,
-                                                opacity:1
-                                            }}
-                                            transition={{
-                                                duration:1,
-                                                type:"spring"
-                                            }}
-                                            key={index} className=" my-2  right flex justify-start px-2 " >
+                                                initial={{
+                                                    x: "-100vw",
+                                                    opacity: 0
+                                                }}
+                                                animate={{
+                                                    x: 0,
+                                                    opacity: 1
+                                                }}
+                                                transition={{
+                                                    duration: 1,
+                                                    type: "spring"
+                                                }}
+                                                key={index} className=" my-2  right flex justify-start px-2 " >
                                                 <div className='    overflow-hidden flex justify-start gap-1 items-start  w-[50%]  text-lg'>
                                                     <img src={i.name} alt="" className=' w-10 bg-white rounded-full  h-10' />
                                                     <p className=' px-2 py-1 bg-black rounded inline-block text-white'>{i.msg}</p> </div>
@@ -112,19 +114,19 @@ const Chat = () => {
                                     else {
                                         return (
                                             <motion.div
-                                            initial={{
-                                                x:"100vw",
-                                                opacity:0
-                                            }}
-                                            animate={{
-                                                x:0,
-                                                opacity:1
-                                            }}
-                                            transition={{
-                                                duration:1,
-                                                type:"spring"
-                                            }}
-                                            key={index} className=" my-2  left flex justify-end px-2 ">
+                                                initial={{
+                                                    x: "100vw",
+                                                    opacity: 0
+                                                }}
+                                                animate={{
+                                                    x: 0,
+                                                    opacity: 1
+                                                }}
+                                                transition={{
+                                                    duration: 1,
+                                                    type: "spring"
+                                                }}
+                                                key={index} className=" my-2  left flex justify-end px-2 ">
                                                 <div className='    overflow-hidden flex justify-end  gap-1 items-start w-[50%]  text-lg'>
                                                     <p className=' px-2 py-1 bg-white rounded inline-block text-black'>{i.msg}</p>
                                                     <img src={i.name} alt="" className=' w-10 bg-white rounded-full h-10' />
@@ -137,7 +139,7 @@ const Chat = () => {
                         </div>
                         <div className="inputbox w-full relative">
                             <input type="text" value={input} placeholder='Type message...' className=' w-full py-2 pl-1 bg-slate-700 rounded-br rounded-bl text-white outline-none' onChange={(e) => setInput(e.target.value)} />
-                            <IoIosSend className=' absolute top-[0.7rem] text-white text-xl right-1' onClick={input===""?()=>alert("Message is required"):handleclick} />
+                            <IoIosSend className=' absolute top-[0.7rem] text-white text-xl right-1' onClick={input === "" ? () => alert("Message is required") : handleclick} />
                         </div>
                     </div>
                 </div>
